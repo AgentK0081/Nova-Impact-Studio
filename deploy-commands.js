c77ac2ef300eacd8ff033372253cfccd677c613f
AgentK0081 <harryjoji08@gmail.com>
2025-11-26 14:16:00 +0530

    Update token environment variable for REST client

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,7 +5,7 @@ const commands = [
   ping.data.toJSON()
 ];
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 try {
   console.log("Uploading slash commands…");
